61eb4ae285c39eeab24938e3fb0a93dac7f3bd6d
HP <ismailkarali661@gmail.com>
2025-12-27 17:21:38 +0300

    A readme file has been added.

--- src/main.java
+++ src/main.java
@@ -1,2 +1,15 @@
-src/main/java/banking
-src/test/java/banking
+# Banking System OOP Project
+
+This project is a simple banking system developed in Java.
+It demonstrates Object-Oriented Programming principles such as:
+        - Inheritance
+- Polymorphism
+- Encapsulation
+- Interfaces
+
+Features:
+        - Deposit
+- Withdraw
+- Transfer
+- Transaction tracking
+- Unit testing with JUnit
