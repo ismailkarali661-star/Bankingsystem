b3fe8a14a2192dac9bbad73e52b662e376dbb65a
HP <ismailkarali661@gmail.com>
2025-12-28 19:43:14 +0300

    There is a Savings Account category.

---
+++ src/banking/model/SavingsAccount.java
@@ -0,0 +1,11 @@
+package banking.model;
+
+/**
+ * Represents a savings account.
+ */
+public class SavingsAccount extends Account {
+
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+}
