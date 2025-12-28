ac401992b35b063d09a8270beb1b472f73d62fda
HP <ismailkarali661@gmail.com>
2025-12-28 19:55:44 +0300

    A processing class was created.

---
+++ src/banking/model/Transaction.java
@@ -0,0 +1,31 @@
+package banking.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * Represents a transaction made on an account.
+ */
+public class Transaction {
+
+    private String type;
+    private double amount;
+    private LocalDateTime timestamp;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
