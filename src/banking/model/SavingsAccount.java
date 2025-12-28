c832fa4c140976a1a5d7829ace11eee99edf318c
HP <ismailkarali661@gmail.com>
2025-12-28 19:45:34 +0300

    Overrode withdraw method in SavingsAccount with minimum balance rule

--- src/banking/model/SavingsAccount.java
+++ src/banking/model/SavingsAccount.java
@@ -5,7 +5,21 @@ package banking.model;
  */
 public class SavingsAccount extends Account {
 
+    private static final double MIN_BALANCE = 100.0;
+
     public SavingsAccount(String accountNumber, double balance) {
         super(accountNumber, balance);
+    }
+
+    /**
+     * Overrides withdraw method to enforce minimum balance rule.
+     */
+    @Override
+    public boolean withdraw(double amount) {
+        if (amount > 0 && (getBalance() - amount) >= MIN_BALANCE) {
+            setBalance(getBalance() - amount);
+            return true;
+        }
+        return false;
     }
 }
